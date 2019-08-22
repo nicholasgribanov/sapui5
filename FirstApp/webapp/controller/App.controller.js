d2315611d9a9133108af355cc3232c945829c407
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-08-22 07:25:45 +0000

    Added i18n support Added model

--- FirstApp/webapp/controller/App.controller.js
+++ FirstApp/webapp/controller/App.controller.js
@@ -6,7 +6,11 @@ sap.ui.define([
 	return Controller.extend("opensap.firstapp.controller.App", {
 		
 		onShowHello: function () {
-			MessageToast.show("Hello!");
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			
+			MessageToast.show(sMsg);
 		}
 	});
 });
