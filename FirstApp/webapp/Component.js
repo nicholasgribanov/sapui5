b645fbb417c5de4e9be47b79563aef7c13d7750f
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-08-26 12:19:17 +0000

    Added Supplier block

--- FirstApp/webapp/Component.js
+++ FirstApp/webapp/Component.js
@@ -1,15 +1,19 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent"
-	], function (UIComponent) {
-		"use strict";
-		
-		return UIComponent.extend("opensap.firstapp.Component", {
-			metadata: {
-				manifest: "json"
-			},
-			
-			init: function () {
-				UIComponent.prototype.init.apply(this,arguments);
-			}
-		});
+], function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("opensap.firstapp.Component", {
+		metadata: {
+			manifest: "json"
+		},
+
+		init: function () {
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// used only for this lessons to show the request individually...
+			this.getModel().setUseBatch(false);
+
+		}
 	});
+});
