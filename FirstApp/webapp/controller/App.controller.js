90a6da80ac191bea2f1143519df4a541c84ba0d2
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-08-26 08:29:14 +0000

    Added searching, filtering and sorting

--- FirstApp/webapp/controller/App.controller.js
+++ FirstApp/webapp/controller/App.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"opensap/firstapp/model/formatter"
-], function (Controller, MessageToast, formatter) {
+	"opensap/firstapp/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, MessageToast, formatter, Filter, FilterOperator) {
 	"use strict";
 	return Controller.extend("opensap.firstapp.controller.App", {
-		
+
 		formatter: formatter,
 
 		onShowHello: function () {
@@ -14,6 +16,19 @@ sap.ui.define([
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
 			MessageToast.show(sMsg);
+		},
+
+		onFilterProducts: function (oEvent) {
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			var oList = this.getView().byId("productsList");
+			var oBinding = oList.getBinding("items");
+
+			if (sQuery) {
+				aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+			}
+			oBinding.filter(aFilter);
 		}
+
 	});
 });
